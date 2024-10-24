5d02039bbcd18bbf940c9ec70018e53bbac22d9f
Yooncheol_Oh <oyc1230@gmail.com>
2024-10-24 22:59:02 +0900

    24.10.24 1. HashMap, windowSlide, twoPointers를 통한 문제풀이

--- src/main/java/org/kr/co/algorithms/hash/Hash.java
+++ src/main/java/org/kr/co/algorithms/hash/Hash.java
@@ -118,4 +118,59 @@ public class Hash {
 
         return answer;
     }
+    
+    /**
+     * @author : 5YoonCheol
+     * @created : 2024-10-24 오후 10:57
+     * @description : S문자열에서 T문자열과 아나그램이 되는 S의 부분문자열의 개수를 구하는 프로그램을 작성하세요.
+     * 아나그램 판별시 대소문자가 구분됩니다. 부분문자열은 연속된 문자열이어야 합니다.
+     */
+    public Integer findAnagram(String a, String b){
+        int answer =0;
+        int lt = 0;
+
+        // a 문자열을 Character 배열로 만들어준다.
+        ArrayList<Character> charList = new ArrayList<>();
+        for (int i = 0; i < a.length(); i++) {
+            charList.add(a.charAt(i));
+        }
+
+        // b 문자열의 구성요소를 HashMap 형태로 저장
+        HashMap<Character, Integer> compareMap = new HashMap<>();
+        for (char x : b.toCharArray()) {
+            compareMap.put(x, compareMap.getOrDefault(x, 0) + 1);
+        }
+
+        // Character 배열의 부분배열을 구하고 compareMap과 일치하면 answer 증가 시킨다.
+        HashMap<Character, Integer> partMap = new HashMap<>();
+
+        // b 문자열의 길이 -1까지 미리 partMap에 세팅 후 시작
+        for(int i=0; i<b.length()-1; i++){
+            partMap.put(charList.get(i), partMap.getOrDefault(charList.get(i), 0) + 1);
+        }
+
+        // 슬라이딩 윈도우 방식으로 부분 문자열을 검사
+        for(int rt=b.length()-1; rt<charList.size(); rt++){
+            // 윈도우의 오른쪽 문자를 추가
+            partMap.put(charList.get(rt), partMap.getOrDefault(charList.get(rt), 0) + 1);
+
+            // 현재 윈도우와 b의 문자 빈도가 동일한지 확인
+            if(partMap.equals(compareMap)){
+                answer++;
+            }
+
+            // 윈도우의 왼쪽 문자를 제거하고 빈도가 0이 되면 삭제
+            partMap.put(charList.get(lt), partMap.get(charList.get(lt)) -1);
+            if(partMap.get(charList.get(lt)) == 0){
+                partMap.remove(charList.get(lt));
+            }
+
+            // 왼쪽 포인터 이동
+            lt++;
+        }
+
+        return answer;
+    }
+    
+    
 }
