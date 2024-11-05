45a56e99d0aef538296e55d161a9858312f648c5
Yooncheol_Oh <oyc1230@gmail.com>
2024-11-05 22:51:12 +0900

    24.11.05 1. Queue를 통한 진료순서 구하기 문제 풀이

---
+++ src/main/java/org/kr/co/algorithms/queue/Person.java
@@ -0,0 +1,10 @@
+package org.kr.co.algorithms.queue;
+
+class Person {
+    int id;
+    int danger;
+    public Person(int id, int danger) {
+        this.id = id;
+        this.danger = danger;
+    }
+}
