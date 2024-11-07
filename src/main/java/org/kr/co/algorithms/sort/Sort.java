05f0ab12fb59e197d779c9f2079a83bef4cf93d2
Yooncheol_Oh <oyc1230@gmail.com>
2024-11-07 21:56:17 +0900

    24.11.07 버블정렬

--- src/main/java/org/kr/co/algorithms/sort/Sort.java
+++ src/main/java/org/kr/co/algorithms/sort/Sort.java
@@ -7,7 +7,7 @@ public class Sort {
      * @description : N개이 숫자가 입력되면 오름차순으로 정렬하여 출력하는 프로그램을 작성하세요.
      * 정렬하는 방법은 선택정렬입니다.
      */
-    public static String selectionSort(int a, int[] arr){
+    public String selectionSort(int a, int[] arr){
         String answer = "";
 
         for(int i=0; i<arr.length-1; i++){
@@ -28,5 +28,32 @@ public class Sort {
         }
         return answer;
     }
+
+    /**
+     * @author : 5YoonCheol
+     * @created : 2024-11-07 오후 9:54
+     * @description : N개이 숫자가 입력되면 오름차순으로 정렬하여 출력하는 프로그램을 작성하세요.
+     * 정렬하는 방법은 버블정렬입니다.
+     */
+    public String bubbleSort(int a, int[] arr){
+        String answer = "";
+
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=0; j<arr.length-i-1; j++){
+                if(arr[j] > arr[j+1]){
+                    // 인접위치와 교환
+                    int tmp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+            }
+        }
+
+        for(int x : arr){
+            answer += x + " ";
+        }
+        return answer;
+    }
+
     
 }
