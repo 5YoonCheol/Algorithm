c49e2dac308af42fd9a57aadf03297cfb95489ab
Yooncheol_Oh <oyc1230@gmail.com>
2024-11-27 19:53:17 +0900

    2024.11.27 재귀함수

--- src/main/java/org/kr/co/algorithms/recursion/Recursion.java
+++ src/main/java/org/kr/co/algorithms/recursion/Recursion.java
@@ -30,10 +30,33 @@ public class Recursion {
     /**
      * @author : 5YoonCheol
      * @created : 2024-11-27 오후 7:15
-     * @description : 자연수 N이 입력되면 N팩토리얼의 결과 값을 출력하는 프로그램을 작성하세요.
+     * @description : 자연수 N이 입력되면 N팩토리얼의 결과 값을 구하는 프로그램을 작성하세요.
      */
     public int Factorial(int n) {
         if(n==1) return 1;
         else return n*Factorial(n-1);
+    }
+
+    /**
+     * @author : 5YoonCheol
+     * @created : 2024-11-27 오후 7:29
+     * @description : 피보나치 수열을 재귀함수를 사용하여 작성하세요.
+     */
+    static int[] fibo;
+    public int Fibonacci(int n) {
+        if(fibo[n]>0) return fibo[n];
+        if (n==1) return 1;
+        else if (n==2) {
+            return 1;
+        }else {
+            return Fibonacci(n-2) + Fibonacci(n-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 45;
+        fibo = new int[n+1];
+        Recursion recursion = new Recursion();
+        for(int i = 0; i <= n; i++){System.out.print(recursion.Fibonacci(n));}
     }
 }
